5909b64ca7887f62bff5b495b33eca414dc70ea7
Laurence <laurencehe5@gmail.com>
2025-02-10 21:03:41 +0000

    Initial commit

--- Pizza/urls.py
+++ Pizza/urls.py
@@ -6,6 +6,6 @@ from .forms import *
 urlpatterns = [
     path('', views.index, name='index'),
     path('logout/', views.logout_view, name='logout'),
-    path('login/', views.login_view, name='login'),
-    path('register/', views.register_view, name='register'),
+    path('login', views.login_view, name='login'),
+    path('signup', views.register_view, name='register'),
 ]
